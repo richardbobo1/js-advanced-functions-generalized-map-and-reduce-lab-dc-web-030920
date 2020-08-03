94e2c3bce296781f66c970b5b3427260cdde07a9
richardbobo1 <richard.bobo@gmail.com>
2020-08-03 11:40:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+
+function map(nums, func ){
+ let x = []
+ for(let i =0; i<nums.length; i++){
+     x.push( func(nums[i]) )
+ }
+ return x 
+}
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
